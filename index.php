4ec5614518160aba464dfc0cc6c18db006a1ceb8
francois80 <glavieux.francois@orange.fr>
2020-01-03 16:59:50 +0100

    correction

--- index.php
+++ index.php
@@ -1,75 +1,87 @@
 <?php
+//tableau contenant le nom des pays
 include "pays.php";
 $affichebloc1 = true;
 $affichebloc2 = true;
-$alertNom= "";
-$alertPrenom = "";
-$alertDate = "";
-$alertAdresse = "";
-$alertCP = "";
+//variables msg d'alerte champs mal saisis
+$firstname = $lastname = $birthdate = $country = $nationality = $address = $zipcode = $city = $mail = $phone = $graduation =
+$poleemploi = $badge = $codecademylink = $hero = $hack = $experience = '';
 
+//regex pour les contrôle du formulaire
 $regexName = "/^[A-Za-zéÉ][A-Za-záàâäãåçéèêëíìîïñóòôöõúùûüýÿæœ]+((-| )[A-Za-záàâäãåçéèêëíìîïñóòôöõúùûüýÿæœ]+)?$/";
 $regexDate = "/^((?:19|20)[0-9]{2})-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/";
 $regexZipCode = "/^((2[AB])|([0-9]{5}))$/";
-$regexAdress = "/\w/";
+$regexAddress = "/\w/";
 $regexTel = "/^0[67](-[0-9]{2}){4}$/";
 $regexPoleEmploi = "/^[0-9]{7}[A-Za-z]$/";
+//tableau d'erreurs
+$errors = [];
 
-if(isset($_POST['firstname']) && empty($_POST['firstname'])){
-    $alertNom = "Veuillez renseigner votre nom";
-}
-
-if( !empty($_POST['firstname'])) {
-    $nom = trim( $_POST['firstname'] );
-    if(! preg_match($regexName, $nom ) ) {
-        $alert =  '<p>Votre nom ne peut pas contenir de caractères spéciaux !</p>';
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $firstname = trim(htmlspecialchars($_POST['firstname']));
+    if(empty($firstname)) {
+        $errors['firstname'] = 'Veuillez renseigner le prénom';
+   }
+   elseif(! preg_match($regexName, $firstname ) ) {
+        $errors['firstname'] =  'Votre prénom contient des caractères non autorisés !';
     }
-} 
-
-if(isset($_POST['lastname']) && empty($_POST['lastname'])){
-    $alertPrenom = "Veuillez renseigner votre prénom";
-}
-
-if( !empty( $_POST['lastname'] ) ) {
-    $prenom = trim( $_POST['lastname'] );
-    if(! preg_match($regexName, $prenom ) ) {
-        $alertNom =  '<p>Votre prénom ne peut pas contenir de caractères spéciaux !</p>';
+    
+    $lastname = trim(htmlspecialchars($_POST['lastname']));
+    if(empty($lastname)) {
+        $errors['lastname'] = 'Veuillez renseigner le nom';
+   }
+   elseif(! preg_match($regexName, $lastname ) ) {
+        $errors['lastname'] =  'Votre nom contient des caractères non autorisés !';
     }
-} 
-
-if(isset($_POST['dateborn']) && empty($_POST['dateborn'])){
-    $alertDate = "Veuillez renseigner votre date de naissance";
-}
-
-if( !empty( $_POST['dateborn'] ) ) {
-    $dateNais = trim( $_POST['dateborn'] );
-    if(! preg_match($regexDate, $dateNais ) ) {
-        $alertDate =  '<p>Votre date de naissance n\'est pas valide !</p>';
+    
+     $birthdate = trim(htmlspecialchars($_POST['birthdate']));
+    if(empty($birthdate)) {
+        $errors['birthdate'] = 'Veuillez renseigner votre date de naissance';
+   }
+   elseif(! preg_match($regexDate, $birthdate ) ) {
+        $errors['birthdate'] =  'Le format valide est aaaa-mm-jj !';
     }
+    
+     $address = trim(htmlspecialchars($_POST['address']));
+    if(empty($address)) {
+        $errors['address'] = 'Veuillez renseigner votre adresse';
+   }
+   elseif(! preg_match($regexAddress, $address ) ) {
+        $errors['address'] =  'Le format de l\'adresse n\'est pas valide!';
+   }
+     $zipcode = trim(htmlspecialchars($_POST['zipcode']));
+    if(empty($zipcode)) {
+        $errors['zipcode'] = 'Veuillez renseigner votre code postal';
+   }
+   elseif(! preg_match($regexZipCode, $zipcode ) ) {
+        $errors['zipcode'] =  'Le format du code postal n\'est pas valide!';
+   }
+    
+    $phone = trim(htmlspecialchars($_POST['phone']));
+    if(empty($phone)) {
+        $errors['phone'] = 'Veuillez renseigner votre téléphone';
+   }
+   elseif(! preg_match($regexTel, $phone ) ) {
+        $errors['phone'] =  'Le format du téléphone n\'est pas valide!';
+   }
+   
+   $poleemploi = trim(htmlspecialchars($_POST['poleemploi']));
+    if(empty($poleemploi)) {
+        $errors['poleemploi'] = 'Veuillez renseigner votre identifiant pole-emploi';
+   }
+   elseif(! preg_match($regexPoleEmploi, $poleemploi ) ) {
+        $errors['poleemploi'] =  'L\' identifiant pole-emploi n\'est pas valide!';
+   }
+   
+   $mail = trim(htmlspecialchars($_POST['mail']));
+    if(empty($mail)) {
+        $errors['mail'] = 'Veuillez renseigner votre email';
+   }
+   elseif(!filter_var($mail, FILTER_VALIDATE_EMAIL)) {
+        $errors['mail'] =  'L\' email  n\'est pas valide!';
+   }
 } 
-
-if(isset($_POST['address']) && empty($_POST['address'])){
-    $alertAdresse = "Veuillez renseigner votre adresse";
-}
-
-if( !empty( $_POST['address'] ) ) {
-    $address = trim( $_POST['address'] );
-    if(! preg_match($regexAdress, $address ) ) {
-        $alertAdresse =  '<p>Votre adresse n\'est pas valide !</p>';
-    }
-} 
-
-if(isset($_POST['cp']) && empty($_POST['cp'])){
-    $alertCP = "Veuillez renseigner votre code postal";
-}
-
-if( !empty( $_POST['cp'] ) ) {
-    $codeP = trim( $_POST['cp'] );
-    if(! preg_match($regexZipCode,$codeP)) {
-        $alertCP =  '<p>Votre code postal n\'est pas valide !</p>';
-    }
-} 
-
+    
 ?>
 <!doctype html>
 <html lang="fr">
@@ -101,10 +113,7 @@ if( !empty( $_POST['cp'] ) ) {
                                 <div class="form-group">
                                     <label for="firstname">Nom : </label>
                                     <input type="text" class="form-control" name="firstname" id="firstname" placeholder="KIROUL" value="<?php if(! empty($_POST['firstname'])){echo $_POST['firstname'];} ?>">
-                                    <?php 
-                                    if ($alertNom != ""){
-                                        echo '<span style="color:red">'. $alertNom. '</span>';
-                                    }?>
+                                    <span class="<?= ! empty($alertNom) ? 'text-danger' : '' ?>"><?= ! empty($alertNom) ?  $alertNom : 'Veuillez renseigner comme dans l\'exemple ci-dessus' ?></span>
                                 </div>
                                 <div class="form-group">
                                     <label for="lastname">Prénom : </label>
@@ -112,8 +121,8 @@ if( !empty( $_POST['cp'] ) ) {
                                     <?php  if($alertPrenom != ""){echo '<span style="color:red">'. $alertPrenom. '</span>';}?>
                                 </div>
                                 <div class="form-group">
-                                    <label for="dateborn">Date de naissance : </label>
-                                    <input type="date" class="form-control" name="dateborn" id="dateborn" value="2018-07-22" min="1900-01-01" max="2020-01-01">
+                                    <label for="birthdate">Date de naissance : </label>
+                                    <input type="date" class="form-control" name="birthdate" id="birthdate" min="1900-01-01" max="2020-01-01">
                                     <?php  if($alertDate != ""){echo '<span style="color:red">'. $alertDate. '</span>';}?>
                                 </div>
                                  <div class="form-group">
@@ -139,10 +148,10 @@ if( !empty( $_POST['cp'] ) ) {
                                     <?php  if($alertAdresse != ""){echo '<span style="color:red">'. $alertAdresse. '</span>';}?>
                                 </div>
                                 <div class="form-group">
-                                    <label for="cp">Code Postal : </label>
-                                    <input type="text" name="cp" id="cp" placeholder="80000" size="5 ">
-                                    <label for="town">Ville : </label>
-                                    <input type="text"  name="town" id="town" placeholder="Amiens" size="40 ">
+                                    <label for="zipcode">Code Postal : </label>
+                                    <input type="text" name="zipcode" id="zipcode" placeholder="80000" size="5 ">
+                                    <label for="city">Ville : </label>
+                                    <input type="text"  name="city" id="city" placeholder="Amiens" size="40 ">
                                     <?php  if($alertCP != ""){echo '<span style="color:red">'. $alertCP. '</span>';}?>
                                 </div>
                             </fieldset>
@@ -152,16 +161,16 @@ if( !empty( $_POST['cp'] ) ) {
                             ?>
                             <div name="bloc2">        
                                 <div class="form-group">
-                                    <label for="address">Email : </label>
-                                    <input type="email" class="form-control" name="email" id="email" placeholder="pierre.kiroul@lamanu.fr">
+                                    <label for="mail">Email : </label>
+                                    <input type="email" class="form-control" name="mail" id="mail" placeholder="pierre.kiroul@lamanu.fr">
                                 </div>
                                 <div class="form-group">
                                     <label for="phone">Téléphone : </label>
                                     <input type="tel" class="form-control" name="phone" id="phone" placeholder="0322950102">
                                 </div>
                                 <div class="form-group">
-                                    <label for="diploma">Diplôme : </label>
-                                    <select name="diploma">
+                                    <label for="graduation">Diplôme : </label>
+                                    <select name="graduation">
                                         <option value="">-- Choisissez --</option>
                                         <option value="sans">Sans</option>
                                         <option value="bac">Bac</option>
@@ -170,28 +179,30 @@ if( !empty( $_POST['cp'] ) ) {
                                     </select>
                                 </div>
                                 <div class="form-group">
-                                    <label for="idpoleemploi">Identifiant pôle-emploi : </label>
-                                    <input type="text" class="form-control" name="idpoleemploi" id="idpoleemploi" placeholder="0224710M">
+                                    <label for="poleemploi">Identifiant pôle-emploi : </label>
+                                    <input type="text" class="form-control" name="poleemploi" id="poleemploi" placeholder="0224710M">
                                 </div>
                                 <div class="form-group">
-                                    <label for="badgenumber">Nombre de badge : </label>
-                                    <input type="number" class="form-control" name="badgenumber" id="badgenumber" min="0" max="10">
+                                    <label for="badge">Nombre de badge : </label>
+                                    <input type="number" class="form-control" name="badge" id="badge" min="0" max="10">
                                 </div>
                                 <div class="form-group">
                                     <a class="text-white-50 font-weight-bold" href="https://www.codecademy.com/">https://www.codecademy.com/</a>
                                 </div>
                                 <div class="form-group">
-                                    <label for="whoyouwanttobe">Si vous étiez un super héros/une super héroïne, qui seriez-vous et pourquoi ? </label>
-                                    <textarea class="form-control" name="whoyouwanttobe" id="whoyouwanttobe" cols="45" row="8" placeholder="Saisissez votre choix ici"></textarea>
+                                    <label for="hero">Si vous étiez un super héros/une super héroïne, qui seriez-vous et pourquoi ? </label>
+                                    <textarea class="form-control" name="hero" id="hero" cols="45" row="8" placeholder="Saisissez votre choix ici"></textarea>
                                 </div>
                                 <div class="form-group">
-                                    <label for="tellus">Racontez-nous un de vos "hacks" (pas forcément technique ou informatique) </label>
-                                    <textarea class="form-control" name="tellus" id="tellus" cols="45" row="8" placeholder="Racontez nous ici"></textarea>
+                                    <label for="hack">Racontez-nous un de vos "hacks" (pas forcément technique ou informatique) </label>
+                                    <textarea class="form-control" name="hack" id="hack" cols="45" row="8" placeholder="Racontez nous ici"></textarea>
                                 </div>
                                 <div class="form-group">
-                                    <label for="radioexperience">Avez vous déjà eu une expérience avec la programmation et/ou l'informatique avant de remplir ce formulaire ? </label><br>
-                                    <input type="radio" name="radioexperience" id="radioexperience" value="oui" /> oui <br>
-                                    <input type="radio" name="radioexperience" id="radioexperience" value="non" /> non <br>
+                                    <p>Avez vous déjà eu une expérience avec la programmation et/ou l'informatique avant de remplir ce formulaire ?</p>
+                                    <label for="experienceoui">oui</label>
+                                    <input type="radio" name="experience" id="experienceoui" value="oui" />
+                                    <label for="experiencenon">non</label>
+                                    <input type="radio" name="experience" id="experiencenon" value="non" />
                                 </div>
                             </div><!-- fin bloc 2 -->
                             <?php
